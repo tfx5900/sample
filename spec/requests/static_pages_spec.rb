ac9933bcf44e836ca2ac0a3276e261931032a951
Khoi Nguyen <me@khoi.vn>
2014-01-21 23:04:51 +0700

    Update spec of home title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,11 +5,9 @@ describe "Static pages" do
   subject { page }
 
   describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    before { visit root_path }    
+    
+    it { should have_title('Home') }
   end
 
   describe "Help page" do
